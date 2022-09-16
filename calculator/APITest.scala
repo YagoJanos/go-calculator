4df69cd991d4bbe14838c36fd79dadad7748c154
Yago Janos <yjquaranta@gmail.com>
2022-09-16 07:11:42 -0300

    Gatling Stress Test script implemented

---
+++ calculator/APITest.scala
@@ -0,0 +1,21 @@
+package com.api.localhost
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class APITest extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+
+  val scn = scenario("APITest")
+    .exec(
+      http("request_0")
+        .get("/history")
+    )
+
+  setUp(scn.inject(atOnceUsers(15000))).protocols(httpProtocol)
+}
